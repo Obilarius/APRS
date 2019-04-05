0d57df7d6e66c75160d54da65d402ddf8935a40a
Obilarius <git@walzenbach.com>
2019-04-05 13:25:30 +0200

    Start with ARPS

---
+++ ARPS/Models/MsSql.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARPS.Classes
+{
+    public class MsSql
+    {
+        public MsSql()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=PC-W10-SW\MSSQLSERVER_DEV;Initial Catalog=ArgesPerm;Integrated Security=True");
+
+
+            //con.Open();
+
+            //string sql = "SELECT ID, Directory FROM dirs";
+            //SqlCommand cmd = new SqlCommand(sql, con);
+
+            //using (SqlDataReader reader = cmd.ExecuteReader())
+            //{
+            //    while (reader.Read())
+            //    {
+            //        var id = reader.GetInt32(0);
+            //        var dir = reader.GetString(1);
+
+            //        allList.Add(new[] { id.ToString(), dir });
+            //    }
+            //}
+        }
+    }
+}
