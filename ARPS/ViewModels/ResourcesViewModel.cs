f0e3a01afbff516b429e64c0d2fe24d9124fe4a0
Obilarius <git@walzenbach.com>
2019-04-08 16:02:29 +0200

    Add Fody (INotifyPropertyChanged)

--- ARPS/ViewModels/ResourcesViewModel.cs
+++ ARPS/ViewModels/ResourcesViewModel.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ARPS.ViewModels
 {
-    public class ResourcesViewModel
+    public class ResourcesViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FolderName { get; set; } = "Name";
+        public string FolderPath { get; set; } = "\\\\Path"; 
+        public string Owner { get; set; } = "Kein Besitzer gefunden";
+
         public ResourcesViewModel()
         {
-            
+            Task.Run(async () =>
+            {
+                int i = 0;
+
+                while (true)
+                {
+                    await Task.Delay(200);
+                    FolderName = (i++).ToString();
+                }
+            });
         }
     }
 }
