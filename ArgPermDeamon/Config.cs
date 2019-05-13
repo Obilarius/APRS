f8d83db4da6b6ae283f52fd58a6c35ba9d129217
Obilarius <git@walzenbach.com>
2019-05-13 12:32:40 +0200

    Alles zur Config in eigene Klasse

---
+++ ArgPermDeamon/Config.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ArgPermDeamon
+{
+    public class Config
+    {
+        /// <summary>
+        /// Prop das den Pfad zur Config Datei hält
+        /// </summary>
+        private string Path = @"config.xml";
+
+        /// <summary>
+        /// Hält die EInträge für jeden einzelnen Server in der Config
+        /// </summary>
+        public List<ConfigServer> Servers { get; set; }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public Config()
+        {
+            Servers = GetServersFromConfig();
+        }
+
+        /// <summary>
+        /// Liest die Config aus und gibt die Liste von Servern zurück
+        /// </summary>
+        List<ConfigServer> GetServersFromConfig()
+        {
+            // Leere RückgabeListe
+            List<ConfigServer> retList = new List<ConfigServer>();
+
+            // Liest die config Datei in ein TextReader Objekt
+            XmlTextReader xtr = new XmlTextReader(Path);
+
+            // Liest jede Zeile der XML Datei
+            while (xtr.Read())
+            {
+                // Wenn die Node ein Element ist, der Name der Node ist "server" und die Node hat 3 Attribute
+                if (xtr.NodeType == XmlNodeType.Element &&
+                    xtr.Name == "server" &&
+                    xtr.AttributeCount == 3)
+                {
+                    // Speichert die einzelnen Attribute in Variablen
+                    string name = xtr.GetAttribute(0);
+                    string displayname = xtr.GetAttribute(1);
+                    ConfigType type = (ConfigType)(Convert.ToInt32(xtr.GetAttribute(2)));
+
+                    retList.Add(new ConfigServer(name, displayname, type));
+                }
+            }
+            return retList;
+        }
+    }
+
+    /// <summary>
+    /// Die Types die in der Config vergeben werden können
+    /// </summary>
+    public enum ConfigType
+    {
+        FileServer = 1
+    }
+
+    /// <summary>
+    /// Die Klasse die aus der Configdatei ausgelesen wird
+    /// </summary>
+    public class ConfigServer
+    {
+        public ConfigServer(string name, string displayName, ConfigType type)
+        {
+            Name = name;
+            DisplayName = displayName;
+            Type = type;
+        }
+
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public ConfigType Type { get; set; }
+    }
+}
--- ArgPermDeamon/Program.cs
+++ ArgPermDeamon/Program.cs
@@ -10,69 +10,29 @@ namespace ARPSDeamon
     {
         static void Main(string[] args)
         {
-            XmlTextReader xtr = new XmlTextReader("config.xml");
-
-            while (xtr.Read())
+            // Prüft ob ein Name für den Server übergeben wurde
+            if (name != null && name.Trim().Length > 0)
             {
-                if (xtr.NodeType == XmlNodeType.Element && 
-                    xtr.Name == "server" && 
-                    xtr.AttributeCount == 3)
+                // Switch über den Type um jeden Type anders zu behandeln
+                switch (type)
                 {
-                    string name = xtr.GetAttribute(0);
-                    string displayname = xtr.GetAttribute(1);
-                    int type = Convert.ToInt32(xtr.GetAttribute(2));
-
-
-                    // Enumerate shares on a remote computer;
-                    if (name != null && 
-                        name.Trim().Length > 0 && 
-                        type == 1)
-                    {
-                        ShareCollection shareColl = ShareCollection.GetShares(name);
-                        if (shareColl != null)
-                        {
-                            foreach (Share share in shareColl)
-                            {
-                                Console.WriteLine("{0}: {1} [{2}] -- {3}", share.ShareType, share.ToString(), share.Path, share.Remark);
-
-
-                                FSWorker.GetSharesSecurity(share, displayname);
-                            }
-                        }
-                    }
-                    
+                    // Type = FileServer
+                    case ConfigType.FileServer:
+                        WorkFileServer(name, displayname, type);
+                        break;
+                    // Default
+                    default:
+                        break;
                 }
+
             }
-
-
-            //    List<string> paths = new List<string>();
-            //paths.Add(@"\\apollon\Administration");
-            //paths.Add(@"\\apollon\Appslab");
-            //paths.Add(@"\\apollon\Archiv");
-            //paths.Add(@"\\apollon\Contacts");
-            //paths.Add(@"\\apollon\Documentation");
-            //paths.Add(@"\\apollon\HardDev");
-            //paths.Add(@"\\apollon\Install");
-            //paths.Add(@"\\apollon\Marketing");
-            //paths.Add(@"\\apollon\Mechanik");
-            //paths.Add(@"\\apollon\Metallography");
-            //paths.Add(@"\\apollon\PM");
-            //paths.Add(@"\\apollon\Production");
-            //paths.Add(@"\\apollon\Public");
-            //paths.Add(@"\\apollon\Purchasing");
-            //paths.Add(@"\\apollon\QMS");
-            //paths.Add(@"\\apollon\REMINST");
-            //paths.Add(@"\\apollon\Sales");
-            //paths.Add(@"\\apollon\SoftDev");
-            //paths.Add(@"\\apollon\User");
-            //paths.Add(@"\\apollon\Vorlagen");
 
 
 
             //foreach (var path in paths)
             //{
-                //DirectoryInfo dInfo = new DirectoryInfo(path);
-                //FSWorker.GetDirectorySecurity(dInfo, -1);
+            //DirectoryInfo dInfo = new DirectoryInfo(path);
+            //FSWorker.GetDirectorySecurity(dInfo, -1);
             //}
 
             //ADWorker.ReadAD();
@@ -83,6 +43,21 @@ namespace ARPSDeamon
         }
 
 
+        
+
+        static void WorkFileServer(string name, string displayname, ConfigType type)
+        {
+            ShareCollection shareColl = ShareCollection.GetShares(name);
+            if (shareColl != null)
+            {
+                foreach (Share share in shareColl)
+                {
+                    FSWorker.GetSharesSecurity(share, displayname);
+                }
+            }
+        }
 
     }
+
+    
 }
